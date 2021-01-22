0d404e7119a19104abb48f84ea1b7c43ea8a7ab5
jon890613 <71360425+jon890613@users.noreply.github.com>
2021-01-22 22:07:16 +0800

    python_get_mysql

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import function
-import time
-if __name__ == "__main__":
+import mysql.connector
+from mysql.connector import Error
 
-    ser = function.setting()
-    fig, ax, line1, data_y1 = function.draw_setting()
-    while 1:
-        function.loop(ser, fig, line1, data_y1)
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='test',
+                                         user='root',
+                                         password='123456')
+    # 使用cursor()方法取得操作指標
+    cursor = connection.cursor()
+
+    # 使用execute方法執行sql陳述式
+    cursor.execute("SELECT VERSION()")
+
+    # 使用 fetchone() 方法取得一條資料
+    data = cursor.fetchone()
+    for i in data:
+        print("Database version : %s " % i)
+
+    connection.close()  # 關閉資料庫連線
+
+except Error as e:
+    print("Error reading data from MySQL table", e)
